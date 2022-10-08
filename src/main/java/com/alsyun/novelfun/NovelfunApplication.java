0d365e349ac1e67454b379f470a6c88831ae58df
Markburt <wangye175005@163.com>
2022-10-09 00:15:02 +0800

    init

---
+++ src/main/java/com/alsyun/novelfun/NovelfunApplication.java
@@ -0,0 +1,13 @@
+package com.alsyun.novelfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelfunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelfunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alsyun/novelfun/NovelfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alsyun.novelfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelfunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
