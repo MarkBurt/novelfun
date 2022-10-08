2883441075353b847f523ddb53f9c500de28e6bc
Markburt <wangye175005@163.com>
2022-10-09 01:21:16 +0800

    add AdminApplication prkject

--- novelfun_admin/src/main/java/com/alsyun/admin/AdminApplication.java
+++ novelfun_admin/src/main/java/com/alsyun/admin/AdminApplication.java
@@ -1,13 +1,11 @@
-package com.alsyun.novelfun;
+package com.alsyun.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NovelfunApplication {
-
+public class AdminApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NovelfunApplication.class, args);
+        SpringApplication.run(AdminApplication.class,args);
     }
-
 }
